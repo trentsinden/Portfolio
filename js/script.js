67d9b499bca03d8d5f60aa8070604346c1cd606d
Trent Sinden <trentsinden@gmail.com>
2020-12-14 15:29:08 -0500

    added five javascript files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Landing Page Text Animation //
+const spans = document.querySelectorAll("h1 span");
+spans.forEach((span) =>
+  span.addEventListener("mouseover", function (e) {
+    span.classList.add("animated", "swing");
+  })
+);
+spans.forEach((span) =>
+  span.addEventListener("mouseout", function (e) {
+    span.classList.remove("animated", "swing");
+  })
+);
+
+// Projects Button Category Change Animation //
+const showRequiredCategory = (event) => {
+  const getId = event.id;
+  const links = document.querySelectorAll(".work-category button");
+  for (i = 0; i < links.length; i++) {
+    if (links[i].hasAttribute("class")) {
+      links[i].classList.remove("active");
+    }
+  }
+
+  event.classList.add("active");
+  const getCategory = document.querySelector(`.category-${getId}`);
+  const categories = document.querySelectorAll('div[class ^= "category-"]');
+  for (i = 0; i < categories.length; i++) {
+    if (categories[i].hasAttribute("class")) {
+      categories[i].classList.remove("showCategory");
+      categories[i].classList.add("hideCategory");
+    }
+  }
+
+  getCategory.classList.remove("hideCategory");
+  getCategory.classList.add("showCategory");
+};
